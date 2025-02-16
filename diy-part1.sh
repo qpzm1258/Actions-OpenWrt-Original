b459031f6c6eae4d9adb6eb7f89660a1f7e56fb8
笑饮砒霜 <qpzm1258@163.com>
2025-02-16 18:37:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -309,6 +309,7 @@ cat >> .config <<EOF
 CONFIG_PACKAGE_luci=y
 CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 CONFIG_LUCI_LANG_zh_Hans=y
+PACKAGE_default-settings-chn=y
 CONFIG_PACKAGE_luci-theme-argon=y
 CONFIG_PACKAGE_luci-app-argon-config=y
 CONFIG_PACKAGE_luci-theme-material=y
